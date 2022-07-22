73520d4f78f264fd6fc76777750974322be51254
AsifVirani5 <asif9263@gmail.com>
2022-07-22 18:18:47 +0530

    This is another commit

---
+++ Importing different file to the current file.py
@@ -0,0 +1,5 @@
+import Class_Public_Private_Variable_Accessibility1
+print(Class_Public_Private_Variable_Accessibility1)
+
+obj4 = Class_Public_Private_Variable_Accessibility1.person()
+print(obj4.age(2022))
